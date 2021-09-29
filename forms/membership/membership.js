72133223e7b1fe1ff34a8376bd64c06d997fe71c
DaBob22 <90647821+DaBob22@users.noreply.github.com>
2021-09-29 14:41:08 -0500

    Save 1

--- forms/membership/membership.js
+++ forms/membership/membership.js
@@ -1,4 +1,15 @@
-/*
-let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
-*/
 
+let members = ["Mary","Sam","Joey","Paul","Mike","Char","Jenny","Kennedy","Kaden","Kurt","Anne"]
+
+
+
+
+submitName.onclick=function(){
+  let memberName = inptName.value
+  if (members.includes(memberName) == true) {
+  lblMember.value = "you are a member!"
+  }
+else { lblMember.value = "You were not a member and have now been added!"
+  members.push(memberName)
+  }
+}
